832eeea5547027b9d30333738fb8b25479b11eee
XUZUL <33335671+SebIannix@users.noreply.github.com>
2019-06-03 21:16:27 +0200

    Updated "sumInv.go" (WIP)

--- GO/Lab04/sumInv.go
+++ GO/Lab04/sumInv.go
@@ -7,21 +7,26 @@ import (
 var in, sum, numLetti int
 
 func main() {
-	fmt.Println("Inserire numeri tra 1 e 9; inserire 0 per terminare il programma:")
-	
+	fmt.Println("	--> Inserire numeri tra 1 e 9; inserire 0 per terminare il programma:")
 	for {
 		fmt.Scan(&in)
 		if in > 0 && in < 10 {
 			sum += in
 			numLetti++
+			numStored := make([]int, numLetti)
+			for v := range numStored {
+				numStored[v] = in
+				
+				}
+			fmt.Println("Slice:", numStored)
 			}
 		if in == 0 {
 			break
 		}
+
 	}
-	
-	
 	
 	fmt.Println("Numeri letti:", numLetti)
 	fmt.Println("Somma finale dei numeri leciti:", sum)
+
 }

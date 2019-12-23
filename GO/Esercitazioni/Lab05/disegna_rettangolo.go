2497fb937198d517b42296a9d61b570642a09df6
XUZUL <33335671+SebIannix@users.noreply.github.com>
2019-12-23 17:48:27 +0100

    Update disegna_rettangolo.go

--- GO/Esercitazioni/Lab05/disegna_rettangolo.go
+++ GO/Esercitazioni/Lab05/disegna_rettangolo.go
@@ -8,10 +8,17 @@ func main() {
 	var r string = "*"
 	var a, b int
 	
-	fmt.Print("Inserire base rettangolo: ")
+	fmt.Print("Inserire base del rettangolo: ")
 	fmt.Scan(&b)
-	fmt.Print("Inserire altezza rettangolo: ")
+	fmt.Print("Inserire altezza del rettangolo: ")
 	fmt.Scan(&a)
+	
+	for a <= 0 || b <= 0 {
+		fmt.Print("Inserire intero positivo come base del rettangolo: ")
+		fmt.Scan(&b)
+		fmt.Print("Inserire intero positivo come altezza del rettangolo: ")
+		fmt.Scan(&a)
+	}
 	
 	for j := 0; j < a; j++ {
 		for i := 0; i < b; i++ {
